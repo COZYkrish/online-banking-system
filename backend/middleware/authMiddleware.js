00df9818f2538342d3b737d0c4d0602481ed663c
COZYkrish <cozykrish2916@gmail.com>
2026-02-02 19:16:57 +0530

    feat: add account and transaction models

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, "SECRET_KEY");
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
