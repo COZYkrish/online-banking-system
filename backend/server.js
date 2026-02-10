bf6adacf9ceb563188ad1c91084db238e27a31c7
COZYkrish <cozykrish2916@gmail.com>
2026-02-10 21:50:34 +0530

    updated app logicx

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,15 @@ const accountRoutes = require("./routes/accountRoutes");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/account", accountRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/account", accountRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Backend is running");
-});
+// app.get("/", (req, res) => {
+//   res.send("Backend is running");
+// });
 
 mongoose
   .connect(process.env.MONGO_URI)
